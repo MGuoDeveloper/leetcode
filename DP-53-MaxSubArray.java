1f8f9b45cf69bd11c698194fd8a1fa151462e59d
Malvin Guo <malvin.guo@nyumc.org>
2019-03-27 12:04:27 -0400

    more

--- DP-53-MaxSubArray.java
+++ DP-53-MaxSubArray.java
@@ -16,16 +16,18 @@ class Solution {
 
 class Solution {
 	public int maxSubArray(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
 		int size = nums.length;
 		int sum = 0;
-		int max = 0;
+		int max = nums[0];
 		for (int i = 0; i < size; i++) {
 			sum += nums[i];
-			if (sum < 0) {
-				sum = 0;
-			}
 			if (sum > max) {
 				max = sum;
+			}
+			if (sum < 0) {
+				sum = 0;
 			}
 		}
 		return max;
