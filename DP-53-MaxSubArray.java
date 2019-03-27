7a7003307bbe0e18582f770bf1c0825c0486c60a
Malvin Guo <malvin.guo@nyumc.org>
2019-03-27 12:08:37 -0400

    more

--- DP-53-MaxSubArray.java
+++ DP-53-MaxSubArray.java
@@ -3,7 +3,7 @@ class Solution {
 		int size = nums.length;
 		int[] sums = new int[size + 1];
 		sums[0] = 0;
-		int res = nums[0] < 0 ? 0 : nums[0];
+		int res = nums[0];
 		for (int i = 0; i < size; i++) {
 			sums[i + 1] = Math.max(sums[i] + nums[i], nums[i]);
 			if (sums[i + 1] > res) {
