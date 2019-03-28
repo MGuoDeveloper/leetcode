c49e995262614529c2d0f9ae747ab64f8ec661f1
Malvin Guo <malvin.guo@nyumc.org>
2019-03-28 16:26:47 -0400

    more

---
+++ DP-97-InterLeaveStr.java
@@ -0,0 +1,105 @@
+class Solution {
+	public boolean isInterleave(String s1, String s2, String s3) {
+		int p = 0; int q = 0; int c = 0;
+		int len1 = s1.length();
+		int len2 = s2.length();
+		int len3 = s3.length();
+		while (c < len1 && p < len2 && q < len3) {
+			char cur = s3.charAt(c);
+			char cp = s1.charAt(p);
+			char cq = s2.charAt(q);
+			if (cp != cur && cq != cur) {
+				return false;
+			} else if (cp == cur && cq != cur) {
+				p++;
+			} else if (cq == cur && cp != cur) {
+				q++;
+			} else {
+				String com = s3.substring(c + 1);
+				return isInterleave(s1.substring(p + 1), s2.substring(q), com) ||
+					isInterleave(s1.substring(p), s2.substring(q + 1), com)
+			}
+		}
+		if (p == len1) return s3.substring(c).equals(s2.substring(q));
+		if (q == len2) return s3.substring(c).equals(s1.substring(p));
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
