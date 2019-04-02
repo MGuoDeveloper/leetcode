defc298aa7da2492702fd183bd198dfdc65d6bbf
Malvin Guo <malvin.guo@nyumc.org>
2019-04-02 14:35:18 -0400

    edit

--- DP-139-WordBreak.java
+++ DP-139-WordBreak.java
@@ -7,7 +7,7 @@ class Solution {
 		boolean[] dp = new boolean[len + 1];
 		dp[0] = true;
 		for (int i = 1; i <= len; i++) {
-			for (int j = i - 1; j >= 0; j++) {
+			for (int j = i - 1; j >= 0; j--) {
 				if (dp[j] && wordDict.contains(s.substring(j, i))) {
 					dp[i] = true;
 					break;
