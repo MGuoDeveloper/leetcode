3d07a17f004ddb4164853b79a01266133b2caa10
Malvin Guo <malvin.guo@nyumc.org>
2019-04-03 10:47:45 -0400

    edit

--- DP-152-MaxProduct.java
+++ DP-152-MaxProduct.java
@@ -1,6 +1,16 @@
 class Solution {
 	public int maxProduct(int[] nums) {
-
+		int front = 1;
+		int back = 1;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length(); i++) {
+			front *= nums[i];
+			back *= nums[nums.length() - i - 1];
+			max = Math.max(max, Math.max(front, back));
+			front = front == 0 ? 1 : front;
+			back = back == 0 ? 1 : back;
+		}
+		return max;
 	}
 }
 
