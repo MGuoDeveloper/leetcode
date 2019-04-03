b742a30941fd20bee6058920c6792a03a1ac9ed3
Malvin Guo <malvin.guo@nyumc.org>
2019-04-03 17:31:39 -0400

    edit

--- DP-213-HouseRob2.java
+++ DP-213-HouseRob2.java
@@ -8,14 +8,12 @@ class Solution {
 			}
 			return max;
 		}
-		return Math.max(helper(nums, 0, len - 2), helper(nums, 0, len - 1))
+		return Math.max(helper(nums, 0, len - 2), helper(nums, 1, len - 1))
 	}
 
 	public int helper(int[] nums, int start, int end) {
 		int len = end - start + 1;
 		int[] dp = new int[len];
-		if (len == 0) return 0;
-		if (len == 1) return nums[start];
 		dp[0] = nums[start];
 		dp[1] = nums[start + 1] > nums[start] ? nums[start + 1] : nums[start];
 		for (int i = start + 2; i <= end; i++) {
