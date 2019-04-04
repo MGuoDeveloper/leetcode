0266730db510b9776fe36d8992bf525df61bba40
Malvin Guo <malvin.guo@nyumc.org>
2019-04-04 10:42:51 -0400

    edit

--- DP-85-MaxRectArea.java
+++ DP-85-MaxRectArea.java
@@ -1,9 +1,10 @@
 class Solution {
-	public int maxReacArea(int[][] matrix) {
+	public int maxReacArea(char[][] matrix) {
 		if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
 			return 0;
 		int m = matrix.length;
 		int n = matrix[0].length;
+		int[] height = new int[n];
 		for (int i = 0; i < n; i++) {
 			height[i] = (matrix[0][i] == '1') ? 1 : 0;
 		}
@@ -15,7 +16,7 @@ class Solution {
 		return res;
 	}
 
-	private void resetHeight(int[][] matrix, int[] height, ind ind) {
+	private void resetHeight(char[][] matrix, int[] height, ind ind) {
 		for (int i = 0; i < height.length; i++) {
 			if (matrix[ind][i] == '1') {
 				height[i] += 1;
@@ -27,14 +28,16 @@ class Solution {
 
 	private int largestInlineHeight(int[] height) {
 		int max = 0;
-		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < height.length; i++) {
+		int len = height.length;
+		Stack<Integer> s = new Stack<>();
+		for (int i = 0; i <= len; i++) {
 			int h = (i == len) ? 0 : height[i];
 			if (s.isEmpty() || h >= height[s.peek()]) {
 				s.push(i);
 			} else {
 				int tp = s.pop();
-				max = Math.max(max, height[tp] * (s.isEmpty() ? i : i - 1 - peek()));
+				max = Math.max(max, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
+				i--;
 			}
 		}
 		return max;
