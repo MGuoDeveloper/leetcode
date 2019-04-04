bd64fb6cb5022b1eb4dc1ff1d9134a9172f56878
Malvin Guo <malvin.guo@nyumc.org>
2019-04-04 17:01:15 -0400

    edit

--- DP-221-MaximalSquare.java
+++ DP-221-MaximalSquare.java
@@ -6,6 +6,27 @@ class Solution {
 		int m = matrix.length;
 		int n = matrix[0].length;
 		int res = 0;
+		int[][] b = new int[m + 1][n + 1];
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (matrix[i - 1][j - 1] == '1') {
+					b[i][j] = Math.min(
+						Math.min(b[i][j - 1], b[i - 1][j - 1]),
+						b[i - 1][j]) + 1;
+					res = Math.max(res, b[i][j]);
+				}
+			}
+		}
+		return res * res;
+	}
+
+	public int maximalSquare(char[][] matrix) {
+		if (matrix == null || matrix.length == 0 ||
+			matrix[0].length == 0)
+			return 0;
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int res = 0;
 		int[][] b = new int[2][n + 1];
 		for (int i = 1; i <= m; i++) {
 			int t = i % 2;
@@ -15,6 +36,8 @@ class Solution {
 						Math.min(b[t][j - 1], b[1 - t][j - 1]),
 						b[1 - t][j]) + 1;
 					res = Math.max(res, b[t][j]);
+				} else {
+					b[t][j] = 0;
 				}
 			}
 		}
